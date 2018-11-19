3742a565efb1d71078eabfc2023eda26bd97176d
yunik1004 <inkyuhak@gmail.com>
2018-11-20 07:54:47 +0900

    Implement module for sequential model

--- frontend/src/components/Scratch.vue
+++ frontend/src/components/Scratch.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    <div id="blockly-div" style="height: 480px; width: 800px;"></div>
-    <xml id="toolbox" style="display: none">
-      <category name="Loops" colour="120">
-        <block type="controls_repeat_ext"></block>
-      </category>
-      <category name="ML" colour="120">
-        <block-component v-for="type in mlBlocks" :type="type" :key="type"></block-component>
-      </category>
-    </xml>
-    <button @click="showCode()">Show code</button>
-    <button @click="runCode()">Run code</button>
+  <div class="container">
+    <div class="row">
+      <div>
+        <p>Result가 보여질 자리</p>
+        <p>Test data: {{testData}} => Expected output: {{testLabels}}</p>
+        <p>Interactive</p>
+      </div>
+    </div>
+    <div class="row">
+      <div  class="col-sm-4">
+        <div>
+          <p>Training data가 들어갈 자리</p>
+          <p>train data: {{trainData}}</p>
+          <p>train labels: {{trainLabels}}</p>
+        </div>
+      </div>
+      <div class="col-sm-8">
+        <div id="blocklyDiv" style="width: 800px; height: 480px;"></div>
+        <xml id="toolbox" style="display: none">
+          <category name="ML" colour="120">
+            <block-component v-for="type in mlBlocks" :type="type" :key="type"></block-component>
+          </category>
+        </xml>
+      </div>
+    </div>
+    <div class="row">
+      <button @click="showCode()">Show code</button>
+      <button @click="runCode()">Run code</button>
+      <button @click="testCode()">Test code</button>
+    </div>
   </div>
 </template>
 
@@ -37,14 +55,31 @@ export default {
   data () {
     return {
       workspace: null,
-      mlBlocks: []
+      mlBlocks: [],
+      model: null,
+      trainData: [],
+      trainLabels: [],
+      testData: [],
+      testLabels: []
     }
   },
   created () {
-    this.mlBlocks.push('test_module')
+    let modules = ['sequentialModel', 'denseLayer', 'train']
+    let traindata = [1, 2, 3, 4, 5]
+    let trainlabels = [1, 3, 5, 7, 9]
+    let testdata = [5, 7]
+
+    // eslint-disable-next-line
+    this.mlBlocks.push.apply(this.mlBlocks, modules)
+    // eslint-disable-next-line
+    this.trainData.push.apply(this.trainData, traindata)
+    // eslint-disable-next-line
+    this.trainLabels.push.apply(this.trainLabels, trainlabels)
+    // eslint-disable-next-line
+    this.testData.push.apply(this.testData, testdata)
   },
   mounted () {
-    this.workspace = Blockly.inject('blockly-div', {
+    this.workspace = Blockly.inject('blocklyDiv', {
       toolbox: document.getElementById('toolbox'),
       scrollbars: false
     })
@@ -65,6 +100,10 @@ export default {
     showCode: function () {
       let code = Blockly.JavaScript.workspaceToCode(this.workspace)
       alert(code)
+    },
+    testCode: function () {
+      // eslint-disable-next-line
+      this.testLabels = this.model.predict(tf.tensor2d(this.testData, [this.testData.length, 1])).dataSync()
     }
   }
 }
--- frontend/src/database/BlockDB.js
+++ frontend/src/database/BlockDB.js
@@ -1,3 +1,5 @@
+import Blockly from 'node-blockly/browser'
+
 let BlockDB = {}
 
 BlockDB['test_module'] = {
@@ -23,6 +25,153 @@ BlockDB['test_module'] = {
         model.predict(tf.tensor2d([5], [1, 1])).print()
       })
     `
+    return code
+  }
+}
+
+BlockDB['sequentialModel'] = {
+  'struct': {
+    'type': 'controls_repeat_ext',
+    'message0': 'Make a model - input size: %1, output size: %2',
+    'args0': [
+      {
+        'type': 'field_number',
+        'name': 'INPUT_SIZE',
+        'value': 1
+      },
+      {
+        'type': 'field_number',
+        'name': 'OUTPUT_SIZE',
+        'value': 1
+      }
+    ],
+    'message1': 'Layers %1',
+    'args1': [
+      {
+        'type': 'input_statement',
+        'name': 'LAYERS'
+      }
+    ],
+    'message2': 'Loss function %1',
+    'args2': [
+      {
+        'type': 'field_dropdown',
+        'name': 'LOSS',
+        'options': [
+          ['meanSquaredError', `'meanSquaredError'`],
+          ['sigmoidCrossEntropy', `'sigmoidCrossEntropy'`],
+          ['softmaxCrossEntropy', `'softmaxCrossEntropy'`]
+        ]
+      }
+    ],
+    'message3': 'Optimizer %1',
+    'args3': [
+      {
+        'type': 'field_dropdown',
+        'name': 'OPTIMIZER',
+        'options': [
+          ['sgd', `'sgd'`],
+          ['momentum', `'momentum'`],
+          ['adagrad', `'adagrad'`],
+          ['adam', `'adam'`],
+          ['rmsprop', `'rmsprop'`]
+        ]
+      }
+    ],
+    'nextStatement': null,
+    'colour': 230,
+    'tooltip': 'Define a model'
+  },
+  'code': function (block) {
+    let code = `var generateSequentialModel = function () {\n`
+
+    code += `const model = tf.sequential()\n`
+
+    let inputSize = block.getFieldValue('INPUT_SIZE')
+    code += `let tb_output_size = ` + inputSize.toString() + `\n`
+
+    let layers = Blockly.JavaScript.statementToCode(block, 'LAYERS').split('\n')
+    for (let i = 0; i < layers.length - 1; i++) {
+      let statements = layers[i].split(';')
+      code += `model.add(` + statements[0] + `)\n`
+      code += statements[1] + `\n`
+    }
+
+    let outputSize = block.getFieldValue('OUTPUT_SIZE')
+    code += `model.add(tf.layers.dense({units:` + outputSize.toString() + `, inputShape: [tb_output_size]}))\n`
+
+    let loss = block.getFieldValue('LOSS')
+    let opt = block.getFieldValue('OPTIMIZER')
+
+    code += `model.compile({loss: ` + loss + `, optimizer: ` + opt + `})\n`
+
+    code += `return model\n`
+    code += `}\n`
+
+    code += `let tbModel = generateSequentialModel()\n`
+    code += `let tbInputSize = ` + inputSize.toString() + `\n`
+
+    return code
+  }
+}
+
+// tf.layers.dense({units: @@@, inputShape: [@@@]}); tb_output_size = @@@\n
+BlockDB['denseLayer'] = {
+  'struct': {
+    'message0': 'Dense layer with %1 unit',
+    'args0': [
+      {
+        'type': 'field_number',
+        'name': 'UNITS',
+        'value': 1
+      }
+    ],
+    'previousStatement': null,
+    'nextStatement': null,
+    'colour': 125,
+    'tooltip': 'Define a dense layer'
+  },
+  'code': function (block) {
+    let units = block.getFieldValue('UNITS')
+    let code = `tf.layers.dense({units:` + units.toString() + `, inputShape: [tb_output_size]});`
+    code += `tb_output_size = ` + units.toString() + `\n`
+    return code
+  }
+}
+
+BlockDB['train'] = {
+  'struct': {
+    'message0': 'Train the model with %1 epochs',
+    'args0': [
+      {
+        'type': 'field_number',
+        'name': 'EPOCHS',
+        'value': 10
+      }
+    ],
+    'previousStatement': null,
+    'colour': 300,
+    'tooltip': 'Train the model'
+  },
+  'code': function (block) {
+    let code = `let tbTrainData = this.trainData\n`
+    code += `let tbTrainLabels = this.trainLabels\n`
+
+    code += `var train = function (model) {\n`
+
+    code += `let tbTrainLength = tbTrainData.length\n`
+
+    code += `const xs = tf.tensor2d(tbTrainData, [tbTrainLength, tbInputSize])\n`
+    code += `const ys = tf.tensor2d(tbTrainLabels, [tbTrainLength, tbInputSize])\n`
+
+    let epochs = block.getFieldValue('EPOCHS')
+    code += `model.fit(xs, ys, {epochs: ` + epochs.toString() + `}).then(() => {\n`
+    code += `alert('Training finished!')\n`
+    code += `})\n`
+    code += `return model\n`
+    code += `}\n`
+
+    code += `this.model = train(tbModel)\n`
     return code
   }
 }
