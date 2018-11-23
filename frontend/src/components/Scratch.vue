35d18e491d9758c92db8a0d296d6ad5054665305
yunik1004 <inkyuhak@gmail.com>
2018-11-24 04:29:49 +0900

    Add resizing function

--- frontend/src/components/Scratch.vue
+++ frontend/src/components/Scratch.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div>
-        <p>Result가 보여질 자리</p>
-        <p>Test data: {{testData}} => Expected output: {{testLabels}}</p>
-        <p>Interactive</p>
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-12">
+          <p>Result가 보여질 자리</p>
+          <p>Test data: {{testData}} => Expected output: {{testLabels}}</p>
+          <p>Interactive</p>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div  class="col-sm-4">
-        <div>
+      <div class="row">
+        <div class="col-sm-12">
+          <button @click="showCode()">Show code</button>
+          <button @click="runCode()">Run code</button>
+          <button @click="testCode()">Test code</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
           <p>Training data가 들어갈 자리</p>
           <p>train data: {{stage.trainData}}</p>
           <p>train labels: {{stage.trainLabels}}</p>
         </div>
-      </div>
-      <div class="col-sm-8">
-        <div id="blocklyDiv" style="width: 800px; height: 480px;"></div>
-        <xml id="toolbox" style="display: none">
-          <category name="ML" colour="120">
-            <block-component v-for="block in stage.blocks" :block="block" :type="block.blockName" :key="block.blockName"></block-component>
-          </category>
-        </xml>
+        <div class="col-sm-8" id="blocklyArea">
+        </div>
       </div>
     </div>
-    <div class="row">
-      <button @click="showCode()">Show code</button>
-      <button @click="runCode()">Run code</button>
-      <button @click="testCode()">Test code</button>
-    </div>
+    <div id="blocklyDiv" style="position: absolute"></div>
+    <xml id="toolbox" style="display: none">
+      <category name="ML" colour="120">
+        <block-component v-for="block in stage.blocks" :block="block" :type="block.blockName" :key="block.blockName"></block-component>
+      </category>
+    </xml>
   </div>
 </template>
 
@@ -69,16 +71,17 @@ export default {
   name: 'Scratch',
   data () {
     return {
-      model: null,
-      testData: [],
-      testLabels: [],
-
       stage: {
         'blocks': [],
         'trainData': [],
         'trainLabels': []
       },
-      loading: 0
+      loading: 0,
+
+      model: null,
+
+      testData: [],
+      testLabels: []
     }
   },
   apollo: {
@@ -106,6 +109,9 @@ export default {
       toolbox: document.getElementById('toolbox'),
       scrollbars: false
     })
+
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
   },
   components: {
     'block-component': blockComponent
@@ -127,6 +133,34 @@ export default {
     testCode: function () {
       // eslint-disable-next-line
       this.testLabels = this.model.predict(tf.tensor(this.testData, [this.testData.length, 1])).dataSync()
+    },
+    onResize: function (e) {
+      let blocklyArea = document.getElementById('blocklyArea')
+      let blocklyDiv = document.getElementById('blocklyDiv')
+
+      // Compute the absolute coordinates and dimensions of blocklyArea.
+      let element = blocklyArea
+      let x = 0
+      let y = 0
+      do {
+        x += element.offsetLeft
+        y += element.offsetTop
+        element = element.offsetParent
+      } while (element)
+
+      // Position blocklyDiv over blocklyArea.
+      blocklyDiv.style.left = x + 'px'
+      blocklyDiv.style.top = y + 'px'
+      blocklyDiv.style.width = blocklyArea.offsetWidth - 1 + 'px'
+
+      let divHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - y - 1
+      if (divHeight < 400) {
+        blocklyDiv.style.height = '400px'
+      } else {
+        blocklyDiv.style.height = divHeight + 'px'
+      }
+
+      Blockly.svgResize(workspace)
     }
   }
 }
