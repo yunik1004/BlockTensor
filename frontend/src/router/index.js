c566d7f17c97be623a5f34cd0909b2bf85f92158
Kenaz03 <ansur0330@gmail.com>
2018-12-03 16:02:57 +0900

    6546546

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Main from '@/components/Main'
 import Scratch from '@/components/Scratch'
 import Scroll from '@/components/Scroll'
-import Select from '@/components/Select'
 import NotFound from '@/components/NotFound'
 import Swal from '@/components/Swal'
 import Template from '@/components/Template'
@@ -16,7 +15,6 @@ export default new Router({
     { path: '/', name: 'Main', component: Main },
     { path: '/scratch', name: 'Scratch', component: Scratch },
     { path: '/Scroll', name: 'Scroll', component: Scroll },
-    { path: '/Select', name: 'Select', component: Select },
     { path: '*', name: 'NotFound', component: NotFound },
     { path: '/Swal', name: 'Swal', component: Swal },
     { path: '/Template', name: 'Template', component: Template }
